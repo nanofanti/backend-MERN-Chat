36a3511115cb0eb267565688af901e8acaeff602
Marco Fanti <fanti.marco8@gmail.com>
2024-07-03 09:23:36 +0200

    cors origin

--- server.js
+++ server.js
@@ -15,18 +15,9 @@ const { app, httpServer } = require("./socket/socket");
 app.use(express.json());
 app.use(cookieParser());
 
-const allowedOrigins = ["https://nanomernchatapp.netlify.app"];
-
 app.use(
   cors({
-    origin: function (origin, callback) {
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        const msg = `The CORS policy for this site does not allow access from the specified Origin.`;
-        return callback(new Error(msg), false);
-      }
-      return callback(null, true);
-    },
+    origin: "https://nanomernchatapp.netlify.app",
     credentials: true,
   })
 );
