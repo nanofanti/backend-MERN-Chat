ff26dd7ff778780364a4ffda6e5de4db6a575dd7
Marco Fanti <fanti.marco8@gmail.com>
2024-07-03 09:13:35 +0200

    updated cors origins

--- socket/socket.js
+++ socket/socket.js
@@ -8,7 +8,7 @@ const httpServer = http.createServer(app);
 
 const io = new Server(httpServer, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://nanomernchatapp.netlify.app",
     methods: ["GET", "POST"],
     credentials: true,
   },
